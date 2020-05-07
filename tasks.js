8fe9798352e3e90f302a62a307b1ca6a8f4f97da
sinclairzx81 <haydn.developer@gmail.com>
2020-05-07 20:39:31 +1200

    parcel 2 | examples refactor

--- tasks.js
+++ tasks.js
@@ -3,10 +3,11 @@ export async function clean() {
     await folder('target').delete().exec()
 }
 
-export async function watch() {  
+// runs an example from the ./examples directory
+export async function example(example) {  
     await shell('tsc -p ./smoke-hub/tsconfig.json --outDir ./target/smoke-hub').exec()
     await Promise.all([
         shell('node ./target/smoke-hub/index.js --port 5001 ').exec(),
-        shell('parcel ./bench/*.html --out-dir ./target/bench --port 5000').exec(),
+        shell(`parcel ./examples/${example}.html --out-dir ./target/examples --port 5000`).exec(),
     ])
 }
